75f3b171b5a59cd61b023e32c1b9ed18518d0f52
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-27 20:31:55 +0530

    updated version

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,72 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Step from "./Step";
 
 const App = () => {
+
+  const [currentStep, setCurrentStep] = useState(1); 
+  // let [previous,setPrevious]=useState(false)
+  // let [next,setNext]=useState(false)
+  let [data,setData]=useState({
+    first_name: "",
+    last_name: "",
+    model: "",
+    car_price: "",
+    card_info: "",
+    expiry_date: "",
+  })
+
+  function handlePrevious(){
+    // setPrevious(true)
+    if(currentStep>1) setCurrentStep(currentStep -1)
+  }
+
+  function handleNext(){
+  // setNext(true)
+  if (currentStep < 3) setCurrentStep(currentStep + 1);
+  // console.log(currentStep)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setData(
+      {
+        first_name:e.target.first_name.value,
+        last_name:e.target.last_name.value,
+        model:e.target.model.value,
+        car_price:e.target.car_price.value,
+        card_info:e.target.card_info.value,
+        expiry_date:e.target.expiry_date.value
+      }
+    )
+    console.log(data)
+  }
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <form action="" onSubmit={handleSubmit} >
+          {
+            currentStep===1 &&
+            <div>
+          <h2>Customer details</h2>
+          <div>
+            <label >First Name:</label><br />
+            <input type="text" name="first_name" id="first_name" value={data.first_name} onChange={(e) => setData({ ...data, first_name: e.target.value })} /><br />
+            <label >Last Name:</label><br />
+            <input type="text" name="last_name" id="last_name" value={data.last_name} onChange={(e) => setData({ ...data, last_name: e.target.value })}  /><br />
+            <button type="button" disabled={currentStep===1}  >Previous</button>
+            <button type="button" onClick={handleNext} >Next</button>
+          </div>
+          </div>
+          }
+          {
+            currentStep>1 && currentStep < 4 && 
+            <Step handlePrevious={handlePrevious}  handleNext={handleNext} currentStep={currentStep} data={data} setData={setData} />
+          } 
+        </form>
     </div>
   )
 }
---
+++ src/components/Step.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+function Step({handlePrevious,handleNext,currentStep,data,setData}) {
+
+    const [errors, setErrors] = useState({})
+     
+    function handleCardInfoChange(e){
+        const { name, value } = e.target;
+        setData({ ...data, [name]: value });
+
+        let errorMessage = '';
+        if (name === 'card_info' && value.length !== 12) {
+            errorMessage = 'Credit card number must be exactly 12 digits long.';
+          }
+
+        if(name=== 'expiry_date' && !/^\d{2}\/\d{2}$/.test(value)){
+            errorMessage = 'Expiration date must be in the format MM/YY';
+        }
+        setErrors({ ...errors, [name]: errorMessage });
+    }
+    
+  return (
+    <div>
+        {
+            currentStep===2 &&(
+            <div>
+                <h2>Car details</h2>
+          <div>
+            <label >Car Model:</label><br />
+            <input type="text" name="model" id="model"  value={data.model} onChange={(e) => setData({ ...data, model: e.target.value })}  /><br />
+            <label >Car Price:</label><br />
+            <input type="number" name="car_price" id="car_price" value={data.car_price} onChange={(e) => setData({ ...data, car_price: e.target.value })}   /><br />
+            <button type="button" onClick={handlePrevious}  >Previous</button>
+            <button type="button" onClick={handleNext}  >Next</button>
+          </div>
+            </div>
+            
+            )
+        }
+
+          { currentStep===3 &&
+            <div>
+            <h2>Card details</h2>
+            <div>
+              <label >Card Info:</label><br />
+              <input type="number" name="card_info" id="card_info"  value={data.card_info} onChange={handleCardInfoChange}  /><br />
+              {errors.card_info && <span style={{ color: 'red' }}>{errors.card_info}</span>}<br />
+              <label >Expiration Date:</label><br /> 
+              <input type="text" name="expiry_date" id="expiry_date"  value={data.expiry_date} onChange={handleCardInfoChange}    /><br />
+              {errors.expiry_date && <span style={{ color: 'red' }}>{errors.expiry_date}</span>}<br />
+              <button type="button" onClick={handlePrevious}  >Previous</button>
+              <button type="submit"    >Submit</button>
+            </div>
+            </div>
+          }
+    </div>
+  )
+}
+
+export default Step
